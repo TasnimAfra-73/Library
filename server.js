1feabf2380cee63c80b973c8e5ae69f30b24718a
Tasnim <tasnimafra@iut-dhaka.edu>
2019-12-21 19:24:57 +0600

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+
 const mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
@@ -13,6 +17,7 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
@@ -20,5 +25,6 @@ db.on("error", error => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose..."));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
